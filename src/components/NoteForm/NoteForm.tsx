3781ff03011b2ae794c440951c68f2c386c1d4f4
Marco Jorge H. Sardido <marcosardido@entropys-MacBook-Pro.local>
2023-04-14 22:55:50 +0800

    component NoteForm: Done

--- src/components/NoteForm/NoteForm.tsx
+++ src/components/NoteForm/NoteForm.tsx
@@ -1,15 +1,39 @@
-import React, { useRef } from 'react'
+import React, { FormEvent, useRef, useState } from 'react'
 import { Button, Col, Form, Row, Stack } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import CreatableReactSelect from "react-select/creatable"
 
-const NoteForm = () => {
+
+// Types
+import { NoteData, Tag } from '../../App'
+
+type NoteFormProps = {
+    onSubmit: (data: NoteData) => void
+}
+
+
+const NoteForm = ({ onSubmit }: NoteFormProps) => {
     // Hooks
     const titleRef = useRef<HTMLInputElement>(null)
     const markdownRef = useRef<HTMLTextAreaElement>(null)
 
+    const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+
+    // Functions
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
+
+        onSubmit({
+            "title": titleRef.current!.value,
+            "markdown": markdownRef.current!.value,
+            "tags": []
+        })
+
+    }
+
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Stack gap={4}>
                 <Row>
                     <Col>
@@ -21,7 +45,16 @@ const NoteForm = () => {
                     <Col>
                         <Form.Group controlId="tags">
                             <Form.Label>Tags</Form.Label>
-                            <CreatableReactSelect isMulti />
+                            <CreatableReactSelect
+                                value={selectedTags.map(tag => {
+                                    return { label: tag.label, value: tag.id }
+                                })}
+                                onChange={tags => {
+                                    setSelectedTags(tags.map(tag => {
+                                        return { label: tag.label, id: tag.value }
+                                    }))
+                                }}
+                                isMulti />
                         </Form.Group>
                     </Col>
                 </Row>
