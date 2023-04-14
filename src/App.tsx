f976234408dcb85e02f839da9aa655106ac2428e
Marco Jorge H. Sardido <marcosardido@entropys-MacBook-Pro.local>
2023-04-14 19:13:27 +0800

    initial routings

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import NewNote from './components/NewNote/NewNote'
+import { Container } from 'react-bootstrap'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Container className='my-4'>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+
+        <Route path="/:id">
+          <Route index element={<h1>Get</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+
+        <Route path="*" element={<h1>404</h1>} />
+      </Routes>
+    </Container>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 )
