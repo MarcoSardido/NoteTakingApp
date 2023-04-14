10a0576b6bc8d7c49f359fe5c387f9776c980a18
Marco Jorge H. Sardido <marcosardido@entropys-MacBook-Pro.local>
2023-04-14 23:08:22 +0800

    create types.ts and importing in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@ import { Route, Routes } from 'react-router-dom'
 import NewNote from './components/NewNote/NewNote'
 import { Container } from 'react-bootstrap'
 
+// Types
+import { RawNote, RawNoteData, Note, NoteData, Tag } from './types'
+
 const App = () => {
+  // Hooks
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
+  // Functions
   return (
     <Container className='my-4'>
       <Routes>
