35b7a548ae6ed1b6f0bd633061045448f9152f17
Marco Jorge H. Sardido <marcosardido@entropys-MacBook-Pro.local>
2023-04-15 15:22:22 +0800

    created help function useNote.tsx

---
+++ src/helper/useNote.tsx
@@ -0,0 +1,10 @@
+import { useOutletContext } from 'react-router-dom'
+import { Note } from '../types'
+
+const useNote = () => {
+  return (
+    useOutletContext<Note>()
+  )
+}
+
+export default useNote
